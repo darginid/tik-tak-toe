e619f2b8275d0d965d70ebd682494f1b6120536f
darginid <dargowork@gmail.com>
2025-04-13 20:00:46 +0300

    start Next.js #23 lesson

--- components/profile/profile.jsx
+++ components/profile/profile.jsx
@@ -7,12 +7,12 @@ export function Profile({ className }) {
     <div
       className={clsx(
         className,
-        "flex items-center gap-2 text-start text-teal-600",
+        "flex items-center gap-3 text-start text-teal-600",
       )}
     >
       <Image src={avatar} width={48} height={48} alt="avatar" unoptimized />
       <div>
-        <p className="text-lg leading-tight">Salikhovfr</p>
+        <p className="text-lg leading-tight">Salikhovfront</p>
         <p className="text-slate-400 text-xs leading-tight">Рейтинг: 1230</p>
       </div>
     </div>
