d6e8147dd29ad0e43863334483dd2686f71caf2e
darginid <dargowork@gmail.com>
2025-06-10 20:22:11 +0300

    JSX 31 Learn Next.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,48 +22,54 @@ export default function HomePage() {
   } = useGameState(playersCount);
 
   return (
+    <HomePageLayout header={<Header />}>
+      <GameTitle playersCount={playersCount} />
+      <GameInfo
+        className="mt-4"
+        playersCount={playersCount}
+        currentMove={currentMove}
+        isWinner={!!winnerSymbol}
+        onPlayerTimeOver={handlePlayerTimeOver}
+      />
+      {winnerSymbol && (
+        <div className="my-4">
+          <GameSymbol symbol={winnerSymbol} />
+        </div>
+      )}
+      <UiModal
+        width="md"
+        isOpen={winnerSymbol}
+        onClose={() => console.log("close")}
+      >
+        <UiModal.Header>Игра Завершена!</UiModal.Header>
+        <UiModal.Body>Hello</UiModal.Body>
+        <UiModal.Footer>
+          <UiButton size="md" variant="outline">
+            Вернуться
+          </UiButton>
+          <UiButton size="md" variant="primary">
+            Играть снова
+          </UiButton>
+        </UiModal.Footer>
+      </UiModal>
+      <GameField
+        className="mt-6"
+        cells={cells}
+        currentMove={currentMove}
+        nextMove={nextMove}
+        handleCellClick={handleCellClick}
+        winnerSequence={winnerSequence}
+        winnerSymbol={winnerSymbol}
+      />
+    </HomePageLayout>
+  );
+}
+
+export function HomePageLayout({ header, children }) {
+  return (
     <div className="bg-slate-50 min-h-screen">
-      <Header />
-      <main className="pt-6 mx-auto w-max">
-        <GameTitle playersCount={playersCount} />
-        <GameInfo
-          className="mt-4"
-          playersCount={playersCount}
-          currentMove={currentMove}
-          isWinner={!!winnerSymbol}
-          onPlayerTimeOver={handlePlayerTimeOver}
-        />
-        {winnerSymbol && (
-          <div className="my-4">
-            <GameSymbol symbol={winnerSymbol} />
-          </div>
-        )}
-        <UiModal
-          width="md"
-          isOpen={winnerSymbol}
-          onClose={() => console.log("close")}
-        >
-          <UiModal.Header>Игра Завершена!</UiModal.Header>
-          <UiModal.Body>Hello</UiModal.Body>
-          <UiModal.Footer>
-            <UiButton size="md" variant="outline">
-              Вернуться
-            </UiButton>
-            <UiButton size="md" variant="primary">
-              Играть снова
-            </UiButton>
-          </UiModal.Footer>
-        </UiModal>
-        <GameField
-          className="mt-6"
-          cells={cells}
-          currentMove={currentMove}
-          nextMove={nextMove}
-          handleCellClick={handleCellClick}
-          winnerSequence={winnerSequence}
-          winnerSymbol={winnerSymbol}
-        />
-      </main>
+      {header}
+      <main className="pt-6 mx-auto w-max">{children}</main>
     </div>
   );
 }
